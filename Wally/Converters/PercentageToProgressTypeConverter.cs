f0d4b5de2bd57d0822e23ac8fc34afad076e6ab5
Florian <0x6664@hey.com>
2020-11-21 09:57:45 +0900

    feat: indeterminate progress bar when erasing / restarting

---
+++ Wally/Converters/PercentageToProgressTypeConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Globalization;
+
+namespace Wally.Converters
+{
+    class PercentageToProgressTypeConverter : BaseValueConverter<PercentageToProgressTypeConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var percentage = (int)value;
+            if (percentage == 0) return true;
+            return false;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
